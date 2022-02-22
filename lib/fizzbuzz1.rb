1286bc17f9e9892360275d1efe58e04b01be8cb4
Catherine Deas <catherine.deas@madetech.com>
2022-02-22 12:10:00 +0000

    Pass  fourth  test

--- lib/fizzbuzz1.rb
+++ lib/fizzbuzz1.rb
@@ -2,10 +2,11 @@ class Fizzbuzz
     def execute(number)
         if number % 3 == 0
             return "fizz"
+        elsif number % 5 == 0
+            return "buzz"
         else
             return number
         end
-
     end
 end
 
