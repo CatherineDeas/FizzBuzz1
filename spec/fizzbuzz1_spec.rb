a61fa0cac9486263cd090a7774f518eab8ac323b
Catherine Deas <catherine.deas@madetech.com>
2022-02-22 11:32:09 +0000

    Fail second test

--- spec/fizzbuzz1_spec.rb
+++ spec/fizzbuzz1_spec.rb
@@ -6,4 +6,10 @@ describe 'fizzbuzz' do
         expect(fizzbuzz.execute(1)).to eq(1)
     end
 
+    it 'returns 2 when passed 2' do
+        fizzbuzz = Fizzbuzz.new
+        expect(fizzbuzz.execute(2)).to eq(2)
+
+    end
+
 end
